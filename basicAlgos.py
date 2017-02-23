3aa56851a8676fffe39cff044965feaf67560014
miteshkumarjha <mitesh@thoughtworks.com>
2017-02-23 15:59:44 +0530

    add function for reversing  a word using while loop

--- basicAlgos.py
+++ basicAlgos.py
@@ -1,3 +1,13 @@
 def add(a,b):
     return a+b;
 print(add(4,3));
+
+def reverse_string(word):
+    reversed_word =""
+    size =len(word)
+    while size:
+        size -=1
+        reversed_word +=word[size]
+    return reversed_word
+
+print (reverse_string("asdf"))
