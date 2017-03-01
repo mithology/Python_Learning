626352ed4645f081119baf11275590f0a86c947d
miteshkumarjha <mitesh@thoughtworks.com>
2017-03-01 15:21:29 +0530

    added factorial function using for loop

--- basicAlgos.py
+++ basicAlgos.py
@@ -9,5 +9,12 @@ def reverse_string(word):
         size -=1
         reversed_word +=word[size]
     return reversed_word
+print (reverse_string("asd sxcxk  djicj djcidj f"))
 
-print (reverse_string("asdf"))
+def factorial_for_whole_number(number):
+    product =1;
+    for i in range(2,number+1):
+        product = product*i;
+    return product
+
+print(factorial_for_whole_number(19))
